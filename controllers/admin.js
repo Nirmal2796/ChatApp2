2ed09bbc3a8f4db3be166efd8060ce3ef2232a75
Nirmal2796 <nirmalgadekar2796@gmail.com>
2023-12-30 21:31:55 +0530

    Retrieving data from databases

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null,title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err=> console.log(err));
+  
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -61,18 +65,25 @@ exports.postEditProduct = (req, res, next) => {
 exports.postDeleteProduct=(req, res, next)=> {
   const prodId = req.body.productId;
 
-  Product.deleteById(prodId);
+  Product.deleteById(prodId)
+    .then(() => {
+      res.redirect('/admin/products');
+    })
+    .catch(err=>console.log(err));
 
-  res.redirect('/admin/products');
+  
 
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([products]) => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      })
+    })
+    .catch(err=>console.log(err));
+  
 };
